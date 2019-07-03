422469806c530600416536d7267ee5f1d9533f85
Matias Saggiorato <matiassaggiorato@gmail.com>
2019-07-03 02:44:00 +0200

    Force cleaning the repo directory so force pushes doesn't create problems.

--- provision/provision-saucal.sh
+++ provision/provision-saucal.sh
@@ -4,23 +4,16 @@ SAUCAL_REPO="/var/provisioners/saucal-custom-site-template/"
 REPO="https://github.com/saucal/custom-site-template.git"
 BRANCH="saucal_version"
 
+rm -rf ${SAUCAL_REPO}
 mkdir -p "${SAUCAL_REPO}"
 
-if [[ -d ${SAUCAL_REPO}/.git ]]; then
-	echo -e "\nUpdating SAU/CAL provisioner in ${SAUCAL_REPO}..."
-	cd ${SAUCAL_REPO}
-	git reset origin/${BRANCH} --hard -q
-	git pull origin ${BRANCH} -q
-	git checkout ${BRANCH} -q
+echo -e "\nDownloading SAU/CAL provisioner, git cloning from ${REPO} into ${SAUCAL_REPO}"
+git clone --recursive --branch ${BRANCH} ${REPO} ${SAUCAL_REPO} -q
+if [ $? -eq 0 ]; then
+	echo "SAU/CAL provisioner clone succesful"
 else
-	echo -e "\nDownloading SAU/CAL provisioner, git cloning from ${REPO} into ${SAUCAL_REPO}"
-	git clone --recursive --branch ${BRANCH} ${REPO} ${SAUCAL_REPO} -q
-	if [ $? -eq 0 ]; then
-		echo "SAU/CAL provisioner clone succesful"
-	else
-		echo "Git failed to clone SAU/CAL provisioner. It tried to clone the ${BRANCH} of ${REPO} into ${SAUCAL_REPO}"
-		exit 1
-	fi
+	echo "Git failed to clone SAU/CAL provisioner. It tried to clone the ${BRANCH} of ${REPO} into ${SAUCAL_REPO}"
+	exit 1
 fi
 
 for VM_DIR in "$@"
